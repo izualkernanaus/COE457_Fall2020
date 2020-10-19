a56a468557b3b131ee1868984435cd7c091942c6
izualkernan <izualkernan@aus.edu>
2020-10-19 10:25:45 +0400

    Oct 19,2020

--- JavaScript/javascript/navigator.js
+++ JavaScript/javascript/navigator.js
@@ -1,12 +1,53 @@
-function show(){
+function show () {
+  let showme = document.querySelector('#show')
 
-    let showme= document.querySelector("#show");
-    showme.innerHTML = "<ul>"+
-    '<ui>'+ "<p>appName  :"+navigator.appName + '</p></ui>'+
-    '<ui>'+"<p>appVersion :"+navigator.appVersion+ '</p></ui>'+
-    '<ui>'+"<p>geolocation  :"+navigator.geolocation+ '</p></ui>'+
-    '<ui>'+"<p>platform  :"+navigator.platform+ '</p></ui>'+
-    '<ui>'+"<p>mediaDevices :"+navigator.mediaDevices+ '</p></ui>'+
-    "</ul>";
+  // grab mediaDevices
 
+  var media_device = '<ul>'
+
+  navigator.mediaDevices.enumerateDevices().then(function (devices) {
+    devices.forEach(function (device) {
+      console.log(
+        device.kind + ': ' + device.label + ' id = ' + device.deviceId
+      )
+      media_device =
+        media_device +
+        '<li>'+
+        device.kind +
+        ': ' +
+        device.label +
+        ' id = ' +
+        device.deviceId+
+        '</li>'
+    })
+
+    media_device = media_device + '</u>';
+
+    console.log('media device:' + media_device)
+
+  
+    showme.innerHTML =
+      '<ul>' +
+      '<ui>' +
+      '<p>appName  :' +
+      navigator.appName +
+      '</p></ui>' +
+      '<ui>' +
+      '<p>appVersion :' +
+      navigator.appVersion +
+      '</p></ui>' +
+      '<ui>' +
+      '<p>geolocation  :' +
+      navigator.geolocation +
+      '</p></ui>' +
+      '<ui>' +
+      '<p>platform  :' +
+      navigator.platform +
+      '</p></ui>' +
+      '<ui>' +
+      '<p>mediaDevices :' +
+      media_device +
+      '</p></ui>' +
+      '</ul>'
+  })
 }
---
+++ jQuery/js/jq.js
@@ -0,0 +1,98 @@
+function change_color() {
+    document.querySelector(".p1").style.color = 'red'
+}
+
+function change_color_jq() {
+    $(".p1").css("color", "blue");
+}
+
+function change_style() {
+    $(".p2").addClass("fancy");
+}
+
+function change_styles() {
+    $(".p2").addClass("fancy wide-margin");
+}
+
+function change_text() {
+    $("#p11").text($("#p23").text());
+}
+
+function change_html() {
+    $("#p11").html("<b>" + $("#p12").text() + "</b>");
+}
+
+function flip() {
+    // if sm then ww
+    if ($(".pic").attr("src") == "img/sm.jpg") {
+        // set the .pic src to img/ww.jpg
+        $(".pic").attr("src", "img/ww.jpg");
+    } else {
+        // ow sm 
+        // set the .pic src to img/sm.jpg
+        $(".pic").attr("src", "img/sm.jpg");
+    }
+}
+
+function add_listener() {
+    $(".pic").click(flip);
+}
+
+function add_keypress() {
+    $(".pic").keydown(
+        function(event) {
+            console.log(event.keyCode);
+            if (event.keyCode == 83) {
+                // set the .pic src to img/sm.jpg
+                $(".pic").attr("src", "img/sm.jpg");
+            } else {
+                // set the .pic src to img/ww.jpg
+                $(".pic").attr("src", "img/ww.jpg");
+            }
+
+        })
+}
+
+
+function add_keypress_document() {
+    $(document).keydown(
+        function(event) {
+            console.log(event.keyCode);
+            if (event.keyCode == 83) {
+                // set the .pic src to img/sm.jpg
+                $(".pic").attr("src", "img/sm.jpg");
+            } else {
+                // set the .pic src to img/ww.jpg
+                $(".pic").attr("src", "img/ww.jpg");
+            }
+
+        })
+}
+
+function add_mouseover() {
+    $(".pic").on("mouseover", flip);
+}
+
+function add_b4() {
+    $("#sm").dblclick(function() {
+        $("#sm").before("<img src ='img/bg.jpg' class='pic'></img>");
+    })
+}
+
+function add_after() {
+    $("#sm").dblclick(function() {
+        $("#sm").after("<img src ='img/bg.jpg' class='pic'></img>");
+    })
+}
+
+function add_append() {
+    $("#sm").dblclick(function() {
+        $("#sm").append("<img src ='img/bg.jpg' class='pic'></img>");
+    })
+}
+
+function add_remove() {
+    $("#sm").dblclick(function() {
+        $("#sm").remove();
+    })
+}
